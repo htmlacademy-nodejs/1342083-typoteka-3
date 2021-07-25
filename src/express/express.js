18c8d6cb77e38b1cb6c2659a9c5f15c51377fd36
extendsnull <extendsnull@gmail.com>
2021-07-25 22:57:37 +0300

    Добавляет выдачу статичных ресурсов

--- src/express/express.js
+++ src/express/express.js
@@ -12,6 +12,8 @@ const app = express();
 app.set(`view engine`, `pug`);
 app.set(`views`, path.resolve(__dirname, ServerConfig.VIEWS_PATH));
 
+app.use(express.static(path.resolve(__dirname, ServerConfig.PUBLIC_PATH)));
+
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
