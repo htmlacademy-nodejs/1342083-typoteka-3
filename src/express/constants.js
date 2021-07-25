cab792200cc6dc45f6ec016816673e4816a06a63
extendsnull <extendsnull@gmail.com>
2021-07-25 22:56:11 +0300

    Настраивает шаблонизацию

--- src/express/constants.js
+++ src/express/constants.js
@@ -2,6 +2,7 @@
 
 const ServerConfig = {
   DEFAULT_PORT: 8080,
+  VIEWS_PATH: `templates`,
 };
 
 module.exports = {ServerConfig};
--- src/express/express.js
+++ src/express/express.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const {ServerConfig} = require(`./constants`);
 const mainRoutes = require(`./routes/main`);
@@ -7,6 +8,10 @@ const myRoutes = require(`./routes/my`);
 const articlesRoutes = require(`./routes/articles`);
 
 const app = express();
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.resolve(__dirname, ServerConfig.VIEWS_PATH));
+
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
