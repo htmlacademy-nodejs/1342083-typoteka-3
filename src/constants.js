f8047506e33ddcab73d07e6fbc40bd68e7c11794
extendsnull <extendsnull@gmail.com>
2021-10-02 18:34:14 +0300

    Добавляет API для поиска

--- src/constants.js
+++ src/constants.js
@@ -22,6 +22,7 @@ const ExitCode = {
 
 const HttpStatusCode = {
   OK: 200,
+  BAD_REQUEST: 400,
   NOT_FOUND: 404,
 };
 
---
+++ src/service/api/search.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const {Router} = require(`express`);
+const {HttpStatusCode} = require(`../../constants`);
+
+const URL = `/search`;
+
+module.exports = (app, service) => {
+  const route = new Router();
+  app.use(URL, route);
+
+  route.get(`/`, (req, res) => {
+    const {query} = req.query;
+    let results = [];
+
+    if (!query.length) {
+      return res.status(HttpStatusCode.BAD_REQUEST).json(results);
+    }
+
+    results = service.findAll(query);
+    return res.status(results.length ? HttpStatusCode.OK : HttpStatusCode.NOT_FOUND).json(results);
+  });
+};
