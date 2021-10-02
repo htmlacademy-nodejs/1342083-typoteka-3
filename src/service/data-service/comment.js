9b4d8252688c36dfd8a8d0e96cb23a5c31062036
extendsnull <extendsnull@gmail.com>
2021-10-02 20:47:11 +0300

    Реализует сервис для комментариев

---
+++ src/service/data-service/comment.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const {getRandomId} = require(`../../utils`);
+
+class CommentService {
+  create(article, comment) {
+    const newComment = {
+      ...comment,
+      id: getRandomId(),
+    };
+    article.comments = [...article.comments, newComment];
+    return newComment;
+  }
+
+  drop(article, id) {
+    const commentToDrop = article.comments.find((comment) => comment.id === id);
+
+    if (!commentToDrop) {
+      return null;
+    }
+
+    const index = article.comments.findIndex((comment) => comment.id === id);
+    article.comments = [...article.comments.slice(0, index), ...article.comments.slice(index + 1)];
+    return commentToDrop;
+  }
+
+  findAll(article) {
+    return article.comments;
+  }
+}
+
+module.exports = CommentService;
--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -2,10 +2,12 @@
 
 const ArticleService = require(`./article`);
 const CategoryService = require(`./category`);
+const CommentService = require(`./comment`);
 const SearchService = require(`./search`);
 
 module.exports = {
   ArticleService,
   CategoryService,
+  CommentService,
   SearchService,
 };
