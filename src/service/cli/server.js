874b2e1126fb2c2e186dc1935bd374adb00064f4
extendsnull <extendsnull@gmail.com>
2021-10-03 19:53:08 +0300

    Добавляет дополнительное логирование для API

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -15,8 +15,25 @@ const NOT_FOUND_TEXT = `404 - Not Found`;
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, api);
-app.use((req, res) => res.status(HttpStatusCode.NOT_FOUND).send(NOT_FOUND_TEXT));
+
+app.use((req, res) => {
+  logger.error(`Route not found: ${req.url}`);
+  res.status(HttpStatusCode.NOT_FOUND).send(NOT_FOUND_TEXT);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 const logger = getLogger({
   name: `api`,
