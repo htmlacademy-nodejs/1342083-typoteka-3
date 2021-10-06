6d78ba440fc26f158d1d91e6fc31a42047663393
extendsnull <extendsnull@gmail.com>
2021-10-06 23:34:15 +0300

    Рефакторит шаблоны, "оживаляет" главную страницу

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const {Router} = require(`express`);
+const {getAPI} = require(`../api`);
 
 const mainRouter = new Router();
+const api = getAPI();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  const categories = await api.getCategories();
+  res.render(`main`, {articles, categories});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`register`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search`));
