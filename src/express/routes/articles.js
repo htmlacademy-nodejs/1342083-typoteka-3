d4819ec84564fc62fb0b3d5154787b7b9f464f64
extendsnull <extendsnull@gmail.com>
2021-10-07 02:24:57 +0300

    Обновляет роут редактирования публикации

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const {Router} = require(`express`);
+const {getAPI} = require(`../api`);
 
 const articlesRouter = new Router();
+const api = getAPI();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`publications-by-category`));
 articlesRouter.get(`/add`, (req, res) => res.render(`admin/add-new-post-empty`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`admin/add-new-post`));
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories(),
+  ]);
+
+  res.render(`admin/edit`, {article, categories});
+});
+
 articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRouter;
