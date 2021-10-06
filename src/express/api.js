309391f303d8118e0a65141acc1ebb151e5bb017
extendsnull <extendsnull@gmail.com>
2021-10-06 23:33:32 +0300

    Добавляет шаблон для API

---
+++ src/express/api.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const axios = require(`axios`);
+
+const TIMEOUT = 1000;
+const port = 3000;
+const apiURL = `http://localhost:${port}/api/`;
+
+class API {
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
+  }
+
+  getArticles() {
+    return this._load(`/articles`);
+  }
+
+  getCategories() {
+    return this._load(`/categories`);
+  }
+}
+
+const defaultAPI = new API(apiURL, TIMEOUT);
+
+module.exports = {
+  API,
+  getAPI() {
+    return defaultAPI;
+  },
+};
